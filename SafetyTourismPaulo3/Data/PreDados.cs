10600562d4125ef58ba738813c41220fb00b8880
Leandro-Caetano <leandromccaetano@upskill.pt>
2020-12-14 15:19:24 +0000

    Dados predefinidos (PreDados)

---
+++ SafetyTourismPaulo3/Data/PreDados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SafetyTourism.Models;
+
+namespace SafetyTourism.Data
+{
+    public static class PreDados
+    {
+        public static void Initialize(SafetyTourismdb context)
+        {
+            //context.Database.EnsureCreated();
+
+            /* Look for any students.
+            if (context.Doencas.Any())
+            {
+                return;   // DB has been seeded
+            }*/
+
+            var doencas = new Doenca[]
+            {
+                new Doenca { NomeDoenca = "Malaria" },
+            };
+
+            context.Doencas.AddRange(doencas);
+            context.SaveChanges();
+        }
+    }
+}
--- SafetyTourismPaulo3/Program.cs
+++ SafetyTourismPaulo3/Program.cs
@@ -6,14 +6,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SafetyTourism.Data; //new
+using Microsoft.Extensions.DependencyInjection; //new
 
 namespace SafetyTourism
 {
     public class Program
     {
+        //new
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        // new
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SafetyTourismdb>();
+                    PreDados.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
